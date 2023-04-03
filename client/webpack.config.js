7686ef65ababfe7a412320f1c47134512c98a647
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-04-03 21:10:55 +0800

    add service worker and htmlwebpack to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { title } = require('process');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
@@ -21,6 +22,15 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+        new HtmlWebpackPlugin({
+          template: './index.html',
+          title: 'Webpack Plugin'
+        }),
+
+        new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'service-worker.js'
+        })
       
     ],
 
